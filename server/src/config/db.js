ab5113163e59027aa266ccdd57465748f55731fc
ritesh harge <ritesh@example.com>
2025-09-10 21:54:47 +0530

    Describe what you changed

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,12 +1,22 @@
 import mongoose from 'mongoose';
+import 'dotenv/config';
 
 export const connectDB = async () => {
-  const uri = process.env.MONGODB_URI ;
+  const uri = process.env.MONGODB_URI;
+  if (!uri) {
+    console.error('❌ MONGODB_URI not defined in environment variables');
+    process.exit(1);
+  }
+
   try {
-    await mongoose.connect(uri, { dbName: 'mern_eats' });
-    console.log('MongoDB connected');
+    await mongoose.connect(uri, {
+      dbName: 'mern_eats',
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('✅ MongoDB connected');
   } catch (err) {
-    console.error('MongoDB connection error', err.message);
+    console.error('❌ MongoDB connection error:', err.message);
     process.exit(1);
   }
 };

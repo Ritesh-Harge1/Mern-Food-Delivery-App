4f4c14e199f51d9c9ba0b1555df7fcb0547dba5d
ritesh harge <ritesh@example.com>
2025-09-10 21:20:08 +0530

    Describe what you changed

--- server/src/index.js
+++ server/src/index.js
@@ -32,7 +32,7 @@ app.use(errorHandler);
 const port = process.env.PORT || 5000;
 
 connectDB().then(() => {
-  app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
+  app.listen(port, () => console.log(`Server running on http://localhost:`));
 });
 
 export default app; // for tests
